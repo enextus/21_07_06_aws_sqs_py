1c6ac4bac6f7653f435d82222a734c9bb6f7b508
enextus <enextus@gmail.com>
2021-07-06 18:57:42 +0200

    21_07_06_

---
+++ createNewBucket.py
@@ -0,0 +1,18 @@
+import boto3
+import botocore
+from random import randint
+
+x = [randint(0, 9) for p in range(0, 10)]
+
+s3 = boto3.resource('s3')
+bucket_name = 'eduard-bucket-00' + chr(x);
+
+def createBucket(bucket_name):
+    if s3.lookup(bucket_name) is None:
+        bucket = s3.create_bucket(bucket_name)  # Bucket Don't Exist
+    else:
+        bucket = s3.get_bucket(bucket_name)  # Bucket Exist
+
+
+if __name__ == "__main__":
+    createBucket(bucket_name)
