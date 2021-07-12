8fa4a1278a1cd5a870b6dce62b2c1f6600de0a7f
enextus <enextus@gmail.com>
2021-07-12 16:24:20 +0200

    21_07_12_

---
+++ checkMyBucket3.py
@@ -0,0 +1,30 @@
+import boto3
+import botocore
+
+s3 = boto3.resource('s3')
+bucket_name = 'eduard-bucket-003'
+bucket = s3.Bucket(bucket_name)
+
+def check_bucket(bucket):
+    try:
+
+        s3.meta.client.head_bucket(Bucket=bucket_name)
+        print("Bucket Exists!")
+        return True
+
+    except botocore.exceptions.ClientError as e:
+
+        # If a client error is thrown, then check that it was a 404 error.
+        # If it was a 404 error, then the bucket does not exist.
+        error_code = int(e.response['Error']['Code'])
+
+        if error_code == 403:
+            print("Private Bucket. Forbidden Access!")
+            return True
+
+        elif error_code == 404:
+            print("Bucket Does Not Exist!")
+            return False
+
+if __name__ == "__main__":
+    check_bucket(bucket)
---
+++ lambdaFunctionTestBucket.py
@@ -0,0 +1,53 @@
+import json
+import boto3
+import botocore
+
+s3 = boto3.resource('s3')
+bucket_name = 'eduard-bucket-00xxxx3'
+bucket = s3.Bucket(bucket_name)
+
+
+def lambda_handler(event, context):
+    try:
+        s3.meta.client.head_bucket(Bucket=bucket_name)
+
+        print("\n" + " " * 4 + "#" * 18)
+        print(" " * 4 + "# Bucket Exists! #")
+        print(" " * 4 + "#" * 18 + "\n")
+
+        # return True
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Hello from Lambda!')
+        }
+
+    except botocore.exceptions.ClientError as e:
+        # If a client error is thrown, then check that it was a 404 error.
+        # If it was a 404 error, then the bucket does not exist.
+
+        error_code = int(e.response['Error']['Code'])
+
+        if error_code == 403:
+            print("\n" + " " * 4 + "#" * 38)
+            print(" " * 4 + "# Private Bucket. Forbidden Access! #")
+            print(" " * 4 + "#" * 38 + "\n")
+
+            return {  # return True
+                'statusCode': error_code,
+                'body': json.dumps('Private Bucket. Forbidden Access! Hello from Lambda! ')
+            }
+
+        elif error_code == 404:
+            print("\n" + " " * 4 + "#" * 26)
+            print(" " * 4 + "# Bucket Does Not Exist! #")
+            print(" " * 4 + "#" * 26 + "\n")
+
+            return {  # return False
+                'statusCode': error_code,
+                'body': json.dumps('Hello from Lambda! Bucket Does Not Exist!')
+            }
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
